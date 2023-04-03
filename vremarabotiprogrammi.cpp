eaf16f50979ca6146e3ec39a5e6f3115ce1852c3
ukl2808 <107134301+ukl2808@users.noreply.github.com>
2023-04-03 11:15:22 +0300

    Update vremarabotiprogrammi.cpp
    Убрал комментарии

--- vremarabotiprogrammi.cpp
+++ vremarabotiprogrammi.cpp
@@ -6,10 +6,10 @@ using namespace chrono;
 
 int main()
 {
-    auto start_time = steady_clock::now(); // запоминаем время начала
-    system("Abstraktclassshape.exe"); // запускаем программу
-    auto end_time = steady_clock::now(); // запоминаем время окончания
-    auto duration = duration_cast<milliseconds>(end_time - start_time); // вычисляем длительность в миллисекундах
+    auto start_time = steady_clock::now();
+    system("Abstraktclassshape.exe");
+    auto end_time = steady_clock::now();
+    auto duration = duration_cast<milliseconds>(end_time - start_time);
     cout << "Programm works " << duration.count() << " milliseconds." << endl;
     return 0;
 }
